139873c83cdc9498b12779a5e8d5a8506b772b0f
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-02-22 22:40:09 +0300

    [ADDED]: get access api key from json file

---
+++ GalleryApp/GalleryMobile/UnsplashAPI/UnsplashAPIClient.cs
@@ -0,0 +1,34 @@
+﻿using GalleryMobile.UnsplashAPI.Model;
+using System.Text.Json;
+
+namespace GalleryMobile.UnsplashAPI
+{
+    public class UnsplashAPIClient
+    {
+        private readonly string KEYS_FILE_NAME = "keys.json";
+        private readonly string BASE_PHOTOS_URI = "https://api.unsplash.com/photos";
+
+        public async Task<string> GetAccessKey()
+        {
+            bool isFileExists = await FileSystem.AppPackageFileExistsAsync(KEYS_FILE_NAME);
+            if (!isFileExists)
+            {
+                return string.Empty;
+            }
+
+            using var stream = await FileSystem.OpenAppPackageFileAsync(KEYS_FILE_NAME);
+            using var reader = new StreamReader(stream);
+
+            string json = await reader.ReadToEndAsync();
+
+            var accessKey = JsonSerializer.Deserialize<ApiKey>(json);
+
+            if (string.IsNullOrWhiteSpace(accessKey?.Key))
+            {
+                return string.Empty;
+            }
+
+            return accessKey.Key;
+        }
+    }
+}
