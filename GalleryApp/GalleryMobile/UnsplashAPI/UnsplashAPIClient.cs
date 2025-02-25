cb8b8007b8a8e106f6245c31c40774f0d966563a
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-02-25 22:58:09 +0300

    [ADDED]: functionality for getting photos from Unsplash API with pagination

--- GalleryApp/GalleryMobile/UnsplashAPI/UnsplashAPIClient.cs
+++ GalleryApp/GalleryMobile/UnsplashAPI/UnsplashAPIClient.cs
@@ -1,5 +1,7 @@
-﻿using GalleryMobile.UnsplashAPI.Model;
+﻿using GalleryMobile.UnsplashAPI.Exceptions;
+using GalleryMobile.UnsplashAPI.Model;
 using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace GalleryMobile.UnsplashAPI
 {
@@ -8,7 +10,7 @@ namespace GalleryMobile.UnsplashAPI
         private readonly string KEYS_FILE_NAME = "keys.json";
         private readonly string BASE_PHOTOS_URI = "https://api.unsplash.com/photos";
 
-        public async Task<string> GetAccessKey()
+        public async Task<string> GetAccessKeyAsync(CancellationToken cancellationToken)
         {
             bool isFileExists = await FileSystem.AppPackageFileExistsAsync(KEYS_FILE_NAME);
             if (!isFileExists)
@@ -19,7 +21,7 @@ namespace GalleryMobile.UnsplashAPI
             using var stream = await FileSystem.OpenAppPackageFileAsync(KEYS_FILE_NAME);
             using var reader = new StreamReader(stream);
 
-            string json = await reader.ReadToEndAsync();
+            string json = await reader.ReadToEndAsync(cancellationToken);
 
             var accessKey = JsonSerializer.Deserialize<ApiKey>(json);
 
@@ -29,6 +31,60 @@ namespace GalleryMobile.UnsplashAPI
             }
 
             return accessKey.Key;
+        }
+
+        public async Task<List<UnsplashPhoto>> GetPhotosAsync(CancellationToken cancellationToken, int page = 1)
+        {
+            string accessKey = await GetAccessKeyAsync(cancellationToken);
+
+            Dictionary<string, int> queryParams = new Dictionary<string, int>
+            {
+                {"per_page", 30},
+                {"page", page }
+            };
+
+            var uriBuilder = new UriBuilder(BASE_PHOTOS_URI)
+            {
+                Query = string.Join("&", queryParams.Select(kvp => $"{kvp.Key}={kvp.Value}"))
+            };
+
+
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Client-ID", accessKey);
+
+            var respone = await httpClient.GetAsync(uriBuilder.Uri, cancellationToken);
+
+            if (!respone.IsSuccessStatusCode)
+            {
+                throw new UnsplashAPIException("There are problems with getting data from Unsplash API");
+            }
+
+            string content = await respone.Content.ReadAsStringAsync(cancellationToken);
+
+            var json = JsonObject.Parse(content);
+
+            var unsplashPhotos = new List<UnsplashPhoto>();
+
+            if (json is JsonArray photos)
+            {
+                foreach (var photo in photos)
+                {
+                    string id = photo["id"].GetValue<string>();
+                    string description = photo["alt_description"].GetValue<string>();
+                    string url = photo["urls"]["regular"].GetValue<string>();
+
+                    var unsplahPhoto = new UnsplashPhoto
+                    {
+                        Id = id,
+                        Description = description,
+                        Url = new Uri(url)
+                    };
+
+                    unsplashPhotos.Add(unsplahPhoto);
+                }
+            }
+
+            return unsplashPhotos;
         }
     }
 }
