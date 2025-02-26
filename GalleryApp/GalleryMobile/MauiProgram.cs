a8533b806c1671c8a0cea259b067838a4a10e114
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-02-26 22:29:22 +0300

    [REFACTORED]: fix dependency injection for main page

--- GalleryApp/GalleryMobile/MauiProgram.cs
+++ GalleryApp/GalleryMobile/MauiProgram.cs
@@ -1,10 +1,14 @@
 ﻿using GalleryMobile.MVVM.ViewModel;
+using GalleryMobile.UnsplashAPI;
 using Microsoft.Extensions.Logging;
+
 
 namespace GalleryMobile
 {
     public static class MauiProgram
     {
+        // Will dispose after application closed ( MAUI dispose services before finish and cancellationTokenSource register in services as singleton(for all app life) )
+        private static readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
 
         public static MauiApp CreateMauiApp()
         {
@@ -18,9 +22,20 @@ namespace GalleryMobile
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            builder.Services.AddSingleton(cancellationTokenSource);
 
-            builder.Services.AddSingleton<MainPage>();
+            builder.Services.AddSingleton<UnsplashAPIClient>();
             builder.Services.AddSingleton<MainPageViewModel>();
+            builder.Services.AddSingleton<ImageDetialsViewModel>(p => new ImageDetialsViewModel(new UnsplashAPI.Model.UnsplashPhoto
+            {
+                /* NOTE: Initialize UnsplashPhoto object with random data to inject */
+                Id = "1",
+                Description = "asdasdasd",
+                Url = new Uri("https://images.unsplash.com/face-springmorning.jpg?q=75&fm=jpg&w=1080&fit=max")
+            }));
+
+
+            builder.Services.AddView<MainPage, MainPageViewModel>();
 
 
 #if DEBUG
@@ -28,6 +43,16 @@ namespace GalleryMobile
 #endif
 
             return builder.Build();
+        }
+
+        private static void AddView<TView, TViewModel>(this IServiceCollection services)
+            where TView : Element, new()
+            where TViewModel : notnull
+        {
+            services.AddSingleton<TView>(serviceProvider => new TView()
+            {
+                BindingContext = serviceProvider.GetRequiredService<TViewModel>()
+            });
         }
 
     }
