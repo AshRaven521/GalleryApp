ed9edeb7d3ebb6800b82a3aaa947b0c524d0bfec
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-02-26 22:31:57 +0300

    [ADDED]: view model for image details with image tapping handling

---
+++ GalleryApp/GalleryMobile/MVVM/ViewModel/ImageDetialsPageViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using GalleryMobile.UnsplashAPI.Model;
+
+namespace GalleryMobile.MVVM.ViewModel
+{
+    public partial class ImageDetialsViewModel : ObservableObject
+    {
+
+        private UnsplashPhoto photo;
+
+        public ImageDetialsViewModel(UnsplashPhoto photo)
+        {
+            this.photo = photo;
+        }
+
+        public UnsplashPhoto Photo
+        {
+            get
+            {
+                return photo;
+            }
+            set
+            {
+                if (photo == value)
+                {
+                    return;
+                }
+                photo = value;
+                OnPropertyChanged(nameof(Photo));
+            }
+        }
+
+        public void ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+
+            var navigatedPhoto = query["Photo"] as UnsplashPhoto;
+            photo.Url = navigatedPhoto.Url;
+            photo.Description = navigatedPhoto.Description;
+            photo.Id = navigatedPhoto.Id;
+        }
+    }
+}
