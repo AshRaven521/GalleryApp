52e9d4513db4d251f873a74deb21e4b10bf03353
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-03-02 23:29:00 +0300

    [ADDED]: handling query attributrs from navigation

--- GalleryApp/GalleryMobile/MVVM/ViewModel/ImageDetialsPageViewModel.cs
+++ GalleryApp/GalleryMobile/MVVM/ViewModel/ImageDetialsPageViewModel.cs
@@ -1,35 +1,55 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using GalleryMobile.UnsplashAPI.Model;
+using System.Collections.ObjectModel;
 
 namespace GalleryMobile.MVVM.ViewModel
 {
     public partial class ImageDetialsViewModel : ObservableObject, IQueryAttributable
     {
 
-        private UnsplashPhoto? photo;
+        private UnsplashPhoto? tappedPhoto;
 
-        public UnsplashPhoto? Photo
+        public UnsplashPhoto? TappedPhoto
         {
             get
             {
-                return photo;
+                return tappedPhoto;
             }
             set
             {
-                if (photo == value)
+                if (tappedPhoto == value)
                 {
                     return;
                 }
-                photo = value;
-                OnPropertyChanged(nameof(Photo));
+                tappedPhoto = value;
+                OnPropertyChanged(nameof(TappedPhoto));
+            }
+        }
+
+        private ObservableCollection<UnsplashPhoto>? navigatedPhotos;
+        public ObservableCollection<UnsplashPhoto>? NavigatedPhotos
+        {
+            get
+            {
+                return navigatedPhotos;
+            }
+            set
+            {
+                if (navigatedPhotos == value)
+                {
+                    return;
+                }
+                navigatedPhotos = value;
+                OnPropertyChanged(nameof(NavigatedPhotos));
             }
         }
 
         public void ApplyQueryAttributes(IDictionary<string, object> query)
         {
-            Photo = (UnsplashPhoto)query["Photo"];
-        }
+            TappedPhoto = (UnsplashPhoto)query["Photo"];
+            NavigatedPhotos = (ObservableCollection<UnsplashPhoto>)query["OtherPhotos"];
 
+        }
     }
 }
