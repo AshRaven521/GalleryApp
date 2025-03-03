7fb1dc889058cf306bd0851e570382f945c1c111
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-03-03 23:30:30 +0300

    [ADDED]: like icon

---
+++ GalleryApp/GalleryMobile/DataPersistence/DataConstants.cs
@@ -0,0 +1,24 @@
+﻿namespace GalleryMobile.DataPersistence
+{
+    public static class DataConstants
+    {
+        private const string DATABASE_NAME = "GalleryApp.db3";
+
+        public const SQLite.SQLiteOpenFlags Flags =
+        // open the database in read/write mode
+        SQLite.SQLiteOpenFlags.ReadWrite |
+        // create the database if it doesn't exist
+        SQLite.SQLiteOpenFlags.Create |
+        // enable multi-threaded database access
+        SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static string DataBasePath
+        {
+            get
+            {
+                string path = Path.Combine(FileSystem.AppDataDirectory, DATABASE_NAME);
+                return path;
+            }
+        }
+    }
+}
---
+++ GalleryApp/GalleryMobile/DataPersistence/IGalleryAppDatabase.cs
@@ -0,0 +1,14 @@
+﻿using GalleryMobile.DataPersistence.Entities;
+using GalleryMobile.UnsplashAPI.Model;
+
+namespace GalleryMobile.DataPersistence
+{
+    public interface IGalleryAppDatabase
+    {
+        Task<User> GetUserByEmailAsync(string email);
+        Task<User> GetUserByNickNameAsync(string nickName);
+        Task<int> SaveUserAsync(User user);
+        Task<List<UnsplashPhoto>> GetLikedPhotosAsync();
+        Task<int> SavePhotoAsync(UnsplashPhoto photo);
+    }
+}
