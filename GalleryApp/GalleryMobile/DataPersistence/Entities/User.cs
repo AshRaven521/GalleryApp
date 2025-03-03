497d31d849510d235362461b802b4ef07ccab380
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-03-03 23:31:21 +0300

    [ADDED: user model

---
+++ GalleryApp/GalleryMobile/DataPersistence/Entities/User.cs
@@ -0,0 +1,40 @@
+﻿using GalleryMobile.UnsplashAPI.Model;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace GalleryMobile.DataPersistence.Entities
+{
+    [Table("users")]
+    public class User
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("user_name")]
+        public string UserName { get; set; } = string.Empty;
+
+        [Column("password")]
+        public string Password { get; set; } = string.Empty;
+
+        [Column("email")]
+        public string Email { get; set; } = string.Empty;
+
+        [OneToMany]
+        [Column("liked_photos")]
+        public List<UnsplashPhoto>? LikedPhotos { get; set; }
+
+        public User()
+        {
+
+        }
+
+        public User(string userName, string password, string email)
+        {
+            UserName = userName;
+            Password = password;
+            Email = email;
+        }
+    }
+}
