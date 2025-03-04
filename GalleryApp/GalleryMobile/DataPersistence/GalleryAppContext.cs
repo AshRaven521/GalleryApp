4e0fed94851aa6ea1822438a6accf8ff962455d7
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-03-05 00:25:45 +0300

    [ADDED: ef core context

---
+++ GalleryApp/GalleryMobile/DataPersistence/GalleryAppContext.cs
@@ -0,0 +1,27 @@
+﻿using GalleryMobile.DataPersistence.Entities;
+using GalleryMobile.UnsplashAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace GalleryMobile.DataPersistence
+{
+    public class GalleryAppContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<UnsplashPhoto> Photos { get; set; }
+
+        public GalleryAppContext()
+        {
+            // The SQLitePCL.Batteries_V2.Init() is needed in the constructor to initiate SQLite on iOS.
+            SQLitePCL.Batteries_V2.Init();
+
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder
+                .UseSqlite($"Filename={DataConstants.DataBasePath}");
+        }
+    }
+}
