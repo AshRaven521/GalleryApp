1789db5571469010aac571e2bdbc4942a85b170a
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-03-05 00:27:12 +0300

    [REFACTORED]: add like photo and log in functionality

--- GalleryApp/GalleryMobile/MVVM/ViewModel/MainPageViewModel.cs
+++ GalleryApp/GalleryMobile/MVVM/ViewModel/MainPageViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using GalleryMobile.DataPersistence.Entities;
+using GalleryMobile.DataPersistence.Services;
 using GalleryMobile.MVVM.View.Pages;
 using GalleryMobile.UnsplashAPI;
 using GalleryMobile.UnsplashAPI.Exceptions;
@@ -8,34 +10,41 @@ using System.Collections.ObjectModel;
 
 namespace GalleryMobile.MVVM.ViewModel
 {
-    public partial class MainPageViewModel : ObservableObject
+    public partial class MainPageViewModel : ObservableObject, IQueryAttributable
     {
         private int pageNumber = 1;
 
         private readonly IUnsplashAPIClient client;
         private readonly CancellationTokenSource cancellationTokenSource;
-
+        private readonly IGalleryAppDatabaseService database;
         private List<UnsplashPhoto> downloadedPhotos;
 
         public MainPageViewModel(IUnsplashAPIClient client,
-                                 CancellationTokenSource cancellationTokenSource)
+                                 CancellationTokenSource cancellationTokenSource,
+                                 IGalleryAppDatabaseService database)
         {
             this.client = client;
             this.cancellationTokenSource = cancellationTokenSource;
+            this.database = database;
             Connectivity.ConnectivityChanged += async (s, e) => await CheckNetworkAccess(s, e);
         }
 
-        private async Task CheckNetworkAccess(object? sender, ConnectivityChangedEventArgs e)
+        private User currentUser;
+
+        public User CurrentUser
         {
-            if (e.NetworkAccess == NetworkAccess.Internet)
+            get
             {
-                await Shell.Current.DisplayAlert("Internet", "Internet Connection Available.", "Ok");
-                downloadedPhotos = await client.GetPhotosAsync(cancellationTokenSource.Token, pageNumber);
-                UnsplashPhotos = new ObservableCollection<UnsplashPhoto>(downloadedPhotos);
+                return currentUser;
             }
-            else
+            set
             {
-                await Shell.Current.DisplayAlert("Internet", "No Internet Connection.", "Ok");
+                if (currentUser == value)
+                {
+                    return;
+                }
+                currentUser = value;
+                OnPropertyChanged(nameof(CurrentUser));
             }
         }
 
@@ -58,14 +67,55 @@ namespace GalleryMobile.MVVM.ViewModel
             }
         }
 
+        /* NOTE: this property change when need to hide like button */
+        private bool isLikeVisible;
+        public bool IsLikeVisible
+        {
+            get
+            {
+                return isLikeVisible;
+            }
+            set
+            {
+                if (isLikeVisible == value)
+                {
+                    return;
+                }
+                isLikeVisible = value;
+                OnPropertyChanged(nameof(IsLikeVisible));
+            }
+        }
+
         [RelayCommand]
         public async Task GetPhotosAsync()
         {
+            IsLikeVisible = false;
             try
             {
+
                 downloadedPhotos = await client.GetPhotosAsync(cancellationTokenSource.Token);
 
+                var likedByUserPhotos = await database.GetUserLikedPhotosAsync(CurrentUser, cancellationTokenSource.Token);
+
+                if (likedByUserPhotos.Any())
+                {
+
+                    foreach (var downloadPhoto in downloadedPhotos)
+                    {
+                        foreach (var userLikedPhoto in likedByUserPhotos)
+                        {
+                            if (downloadPhoto.Id == userLikedPhoto.Id)
+                            {
+                                downloadPhoto.IsLiked = userLikedPhoto.IsLiked;
+                            }
+                        }
+                    }
+                }
+
+
                 UnsplashPhotos = new ObservableCollection<UnsplashPhoto>(downloadedPhotos);
+
+                IsLikeVisible = true;
             }
             catch (UnsplashAPIException)
             {
@@ -76,6 +126,7 @@ namespace GalleryMobile.MVVM.ViewModel
         [RelayCommand]
         public async Task RemainingItemsThresholdReachedAsync()
         {
+            IsLikeVisible = false;
             // Firstly increment because first page loaded with application loaded (or button pressed)
             pageNumber++;
             try
@@ -85,6 +136,7 @@ namespace GalleryMobile.MVVM.ViewModel
                 {
                     UnsplashPhotos.Add(photo);
                 }
+                IsLikeVisible = true;
             }
             catch (UnsplashAPIException)
             {
@@ -102,8 +154,68 @@ namespace GalleryMobile.MVVM.ViewModel
                 {"OtherPhotos", UnsplashPhotos }
             };
 
-            await Shell.Current.GoToAsync(nameof(ImageDetails), navigationParameter);
+            await Shell.Current.GoToAsync(nameof(ImageDetailsPage), navigationParameter);
 
+        }
+
+        [RelayCommand]
+        public async Task LikePhotoAsync(UnsplashPhoto likePhoto)
+        {
+            var foundPhoto = UnsplashPhotos.FirstOrDefault(x => x.Id == likePhoto.Id);
+
+            if (foundPhoto.IsLiked.Value)
+            {
+                foundPhoto.UserId = null;
+                foundPhoto.IsLiked = false;
+                CurrentUser.LikedPhotos.Remove(foundPhoto);
+            }
+            else
+            {
+                foundPhoto.UserId = CurrentUser.Id;
+                foundPhoto.IsLiked = true;
+                CurrentUser.LikedPhotos.Add(foundPhoto);
+            }
+
+            await database.SavePhotoAsync(foundPhoto, cancellationTokenSource.Token);
+            await database.SaveUserAsync(CurrentUser, cancellationTokenSource.Token);
+        }
+
+        [RelayCommand]
+        public async Task LogOutAsync()
+        {
+            CurrentUser.IsLoggedIn = false;
+            await database.SaveUserAsync(CurrentUser, cancellationTokenSource.Token);
+
+            var navParams = new Dictionary<string, object>
+            {
+                {"IsFirstLoad",  false}
+            };
+
+            await Shell.Current.GoToAsync($"///{nameof(LogInPage)}", navParams);
+        }
+        public void ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            CurrentUser = (User)query["CurrentUser"];
+        }
+
+        private async Task CheckNetworkAccess(object? sender, ConnectivityChangedEventArgs e)
+        {
+            if (e.NetworkAccess == NetworkAccess.Internet)
+            {
+                IsLikeVisible = false;
+
+                await Shell.Current.DisplayAlert("Internet", "Internet Connection Available.", "Ok");
+                downloadedPhotos = await client.GetPhotosAsync(cancellationTokenSource.Token, pageNumber);
+                UnsplashPhotos = new ObservableCollection<UnsplashPhoto>(downloadedPhotos);
+
+                IsLikeVisible = true;
+            }
+            else
+            {
+                IsLikeVisible = false;
+
+                await Shell.Current.DisplayAlert("Internet", "No Internet Connection.", "Ok");
+            }
         }
 
     }
