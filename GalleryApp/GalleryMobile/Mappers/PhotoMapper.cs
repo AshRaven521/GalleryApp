34f8457b85010ba96863d9ce9f18bb9b22064414
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-03-06 20:13:08 +0300

    [ADDED]: mapper from ui to db photo model

---
+++ GalleryApp/GalleryMobile/Mappers/PhotoMapper.cs
@@ -0,0 +1,20 @@
+﻿using GalleryMobile.DataPersistence.Entities;
+using GalleryMobile.UnsplashAPI.Model;
+
+namespace GalleryMobile.Mappers
+{
+    public static class PhotoMapper
+    {
+        public static ThumbnailPhoto MapUnsplashToThumbnail(UnsplashPhoto unsplashPhoto)
+        {
+            var thumbPhoto = new ThumbnailPhoto();
+
+            thumbPhoto.ApiId = unsplashPhoto.ApiId;
+            thumbPhoto.Description = unsplashPhoto.Description;
+            thumbPhoto.Url = unsplashPhoto.Url;
+            thumbPhoto.Users = unsplashPhoto.Users;
+
+            return thumbPhoto;
+        }
+    }
+}
